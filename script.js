7dbdac23a1234460af84807f10749332109f0fdc
Renato Silva <renato.oliveira.silva@gmail.com>
2020-09-02 18:04:26 -0300

    Better navigation Improved descriptions

--- script.js
+++ script.js
@@ -2,29 +2,31 @@
     var app = new Vue({
   el: '#app',
   data: {
-    message: 'Hello Vue!',
-    brand: "Zendesk Brand",
+    brand: "Zendesk",
     menu: [
         {
-            label: "SunCo ",
+            id: "sunco-cfc",
+            label: "SunCo CFC",
             url: "sunco.html",
-            description: "Duis mollis, est non commodo luctus, nisi erat porttitor ligula",
-            docs_url: "",
-            logo: "https://docs.smooch.io/guide/web-messenger/"
+            description: "Sunshine Conversations makes it easy to add conversational capabilities to your product or business. In these docs you’ll find everything you need to use the platform.",
+            docs_url: "https://docs.smooch.io/guide/web-messenger/",
+            logo: ""
         },
         {
+            id: "sunco-legacy",
             label: "SunCo Legacy",
             url: "sunco-legacy.html",
-            description: "Duis mollis, est non commodo luctus, nisi erat porttitor ligula",
-            docs_url: "",
-            logo: "https://docs.smooch.io/guide/web-messenger/"
+            description: "Sunshine Conversations makes it easy to add conversational capabilities to your product or     business. In these docs you’ll find everything you need to use the platform.",
+            docs_url: "https://docs.smooch.io/guide/web-messenger/",
+            logo: ""
         },
         {
+            id: "support",
             label: "Support",
             url: "support.html",
-            description: "Duis mollis, est non commodo luctus, nisi erat porttitor ligula",
-            docs_url: "",
-            logo: "https://developer.zendesk.com/embeddables/docs/widget/introduction"
+            description: "You can embed the Web Widget in any website to provide your customers with Zendesk functionality such as ticketing, live chat, talk, and Help Center content.",
+            docs_url: "https://developer.zendesk.com/embeddables/docs/widget/introduction",
+            logo: ""
         }
     ]
   }
